148a1914c8fadd0ff246810a75e2cd32fd1d5281
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-09-23 12:48:20 +0200

    update

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description…" />
+    <div className="min-w-xl space-y-3">
+      <TextField.Root placeholder="Title">
+        <TextField.Slot></TextField.Slot>
+      </TextField.Root>
+      <TextArea placeholder="description" />
       <Button>Submit New Issue</Button>
     </div>
   );
 };
+
 export default NewIssuePage;
